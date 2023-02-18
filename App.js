8fab05f80f15378b6b75fc8863db035b7df75b83
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-02-19 00:11:25 +0530

    initial config

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [loaded] = useFonts({
+    ExtraBold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    Medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    Regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    SemiBold: require("./assets/fonts/Poppins-SemiBold.ttf"),
+  });
+  if(!loaded) return null;
+  return(
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}
+        initialRouteName = 'Home'
+      >
+        <Stack.Screen name = "Home" component = {Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
