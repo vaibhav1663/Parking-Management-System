0aa4a5d0baea5d59e893a8053d120411d75b0957
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-03-27 15:01:03 +0530

    added backend

--- src/data.js
+++ src/data.js
@@ -1,51 +1,4 @@
 export const sensorData = [
-<<<<<<< HEAD
-    {
-        _id: 1,
-        status: 1,
-        
-    },
-    {
-        _id: 2,
-        status: 0,
-    },
-    {
-        _id: 3,
-        status: 1,
-        
-    },
-    {
-        _id: 4,
-        status: 1,
-        
-    },
-    {
-        _id: 5,
-        status: 0,
-
-    },
-    {
-        _id: 6,
-        status: 0,
-
-    },
-]
-
-export const bgColor = (status) =>{
-    if(status===1){
-        return 'green';
-    }
-    else{
-        return 'red';
-    }
-}
-
-// const bookSlot = (slotId) => {
-//     sensorData.find(_id === slotId).then((data) => {
-//         console.log(data); 
-//     })
-// }
-=======
   {
     _id: 1,
     status: 1,
@@ -85,4 +38,3 @@ export const bgColor = (status) => {
     return "red";
   }
 };
->>>>>>> 6708bade4c87173f35abc06fca08acc1026b24c4
